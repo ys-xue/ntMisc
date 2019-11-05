1d96ba16758fddba1267226598764a326beaf5a7
Yishu Xue <yishu.xue@uconn.edu>
2019-11-04 22:15:31 -0500

    initial commit

---
+++ R/dahl.R
@@ -0,0 +1,24 @@
+##' Dahl's Method for MCMC Inference
+##'
+##' @usage dahl(mcmc.object)
+##'
+##' @param mcmc.object an MCMC object
+##'
+##' return a list:
+##' \itemize{
+##' \item min.sse the smallest SSE
+##' \item cLS the index for the iteration that minimize SSE
+##' \item cluster the final clustering outcome
+##' }
+##'
+##' @importFrom purrr map map_dbl
+##' @export
+
+dahl <- function(mcmc.object) {
+  niter <- nrow(mcmc.object)
+  BList <- map(1:niter, ~outer(mcmc.object[.x,], mcmc.object[.x,], "=="))
+  BBar <- Reduce("+", "BList") / niter
+  SSE <- map_dbl(BList, ~sum((.x - BBar)^2))
+  return(list(min.sse = min(SSE), cLS = which.min(SSE),
+              cluster = as.numeric(mcmc.object[which.min(SSE),])))
+}
